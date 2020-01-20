1ca767296ef7717328b513c7bf7678b049903911
Wesley Webster <wesleywebster91@gmail.com>
2020-01-20 01:44:30 -0600

    black eagle images

--- src/House/index.js
+++ src/House/index.js
@@ -1,18 +1,22 @@
-// Golden Deer
-// import claude from './GoldenDeerHouse/Claude_thumb.png';
-// import hilda from './GoldenDeerHouse/Hilda_thumb.png';
-// import leonie from './GoldenDeerHouse/Leonie_thumb.png';
-// import lorenz from './GoldenDeerHouse/Lorenz_thumb.png';
-// import lysithea from './GoldenDeerHouse/Lysithea_thumb.png';
-// import raphael from './GoldenDeerHouse/Raphael_thumb.png';
-// import marianne from './GoldenDeerHouse/Marianne_thumb.png';
-// import ignatz from './GoldenDeerHouse/Ignatz_thumb.png';
+// Black Eagles
+export const edelgard = require(`./Black_Eagles_House/Edelgard_thumb.png`);
+export const hubert = require(`./Black_Eagles_House/Hubert_thumb.png`);
+export const ferdinand = require(`./Black_Eagles_House/Ferdinand_thumb.png`);
+export const petra = require(`./Black_Eagles_House/Petra_thumb.png`);
+export const caspar = require(`./Black_Eagles_House/Caspar_thumb.png`);
+export const linhardt = require(`./Black_Eagles_House/Linhardt_thumb.png`);
+export const bernadetta = require(`./Black_Eagles_House/Bernadetta_thumb.png`);
+export const dorothea = require(`./Black_Eagles_House/Dorothea_thumb.png`);
 
+// Golden Deer
 export const claude = require(`./GoldenDeerHouse/Claude_thumb.png`);
 export const hilda = require(`./GoldenDeerHouse/Hilda_thumb.png`);
-export const leonie = require(`./GoldenDeerHouse/Leonie_thumb.png`)
-export const lorenz= require(`./GoldenDeerHouse/Lorenz_thumb.png`)
-export const lysithea= require(`./GoldenDeerHouse/Lysithea_thumb.png`)
-export const raphael= require(`./GoldenDeerHouse/Raphael_thumb.png`)
-export const marianne= require(`./GoldenDeerHouse/Marianne_thumb.png`)
-export const ignatz= require(`./GoldenDeerHouse/Ignatz_thumb.png`)
+export const leonie = require(`./GoldenDeerHouse/Leonie_thumb.png`);
+export const lorenz= require(`./GoldenDeerHouse/Lorenz_thumb.png`);
+export const lysithea= require(`./GoldenDeerHouse/Lysithea_thumb.png`);
+export const raphael= require(`./GoldenDeerHouse/Raphael_thumb.png`);
+export const marianne= require(`./GoldenDeerHouse/Marianne_thumb.png`);
+export const ignatz= require(`./GoldenDeerHouse/Ignatz_thumb.png`);
+
+// Blue Lions
+export const dimitri = require(`./Blue_Lions_House/Dimitri_thumb.png`);
