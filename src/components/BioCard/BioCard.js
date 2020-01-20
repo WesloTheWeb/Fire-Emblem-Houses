1e9791c0a97fe2197e479f2d72f6707cb2345043
Wesley Webster <wesleywebster91@gmail.com>
2020-01-20 03:07:45 -0600

    colors

--- src/components/BioCard/BioCard.js
+++ src/components/BioCard/BioCard.js
@@ -7,13 +7,13 @@ const BioCard = (props) => {
     return (
         <div className={classes.BioCard}>
             <p>{props.image}</p>
-            <p>{props.name}</p>
-            <p><b>Age:</b> {props.age}</p>
-            <p><b>Height:</b> {props.age}</p>
-            <p><b>House:</b> {props.house}</p>
-            <p><b>Crest:</b> {props.crest}</p>
-            <p><b>Likes:</b> {props.likes}</p>
-            <p><b>Dislikes:</b> {props.dislikes}</p>
+            <p className={classes.studentInfo}>{props.name}</p>
+            <p><b>Age:</b> <span className={classes.studentInfo}>{props.age}</span></p>
+            <p><b>Height:</b> <span className={classes.studentInfo}>{props.height}</span></p>
+            <p><b>House:</b> <span className={classes.studentInfo}>{props.house}</span></p>
+            <p><b>Crest:</b> <span className={classes.studentCrest}>{props.crest}</span></p>
+            <p><b>Likes:</b> <span className={classes.like}>{props.likes}</span></p>
+            <p><b>Dislikes:</b> <span className={classes.dislike}>{props.dislikes}</span></p>
         </div>
     );
 }
