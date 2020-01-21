7886af9d9cea274a7552b44b58a71016c11d7949
Wesley Webster <wesleywebster91@gmail.com>
2020-01-21 02:24:21 -0600

    destructured CSS
    Destructured some CSS

--- src/components/BioCard/BioCard.js
+++ src/components/BioCard/BioCard.js
@@ -4,18 +4,19 @@ import classes from './biocard.css';
 
 
 const BioCard = (props) => {
-    const {image, name, age, height, house, crest, likes, dislikes} = props
+    const {image, name, age, height, house, crest, likes, dislikes} = props;
+    const {studentInfo, BioCard, studentCrest, like, dislike} = classes;
     
     return (
-        <div className={classes.BioCard}>
+        <div className={BioCard}>
             <p>{image}</p>
-            <p className={classes.studentInfo}>{name}</p>
-            <p><b>Age:</b> <span className={classes.studentInfo}>{age}</span></p>
-            <p><b>Height:</b> <span className={classes.studentInfo}>{height}</span></p>
-            <p><b>House:</b> <span className={classes.studentInfo}>{house}</span></p>
-            <p><b>Crest:</b> <span className={classes.studentCrest}>{crest}</span></p>
-            <p><b>Likes:</b> <span className={classes.like}>{likes}</span></p>
-            <p><b>Dislikes:</b> <span className={classes.dislike}>{dislikes}</span></p>
+            <p className={studentInfo}>{name}</p>
+            <p><b>Age:</b> <span className={studentInfo}>{age}</span></p>
+            <p><b>Height:</b> <span className={studentInfo}>{height}</span></p>
+            <p><b>House:</b> <span className={studentInfo}>{house}</span></p>
+            <p><b>Crest:</b> <span className={studentCrest}>{crest}</span></p>
+            <p><b>Likes:</b> <span className={like}>{likes}</span></p>
+            <p><b>Dislikes:</b> <span className={dislike}>{dislikes}</span></p>
         </div>
     );
 }
